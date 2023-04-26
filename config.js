b46236dbc1dacaca72bb7ec394a44f48e2146d24
Pranav K <pranavkcse@gmail.com>
2023-04-26 20:55:05 +0530

    vite js multi page

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/about.html'),
+        nested: resolve(__dirname, 'about/index.html'),
       },
     },
   },

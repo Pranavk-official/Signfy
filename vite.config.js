fd1a4377d732c427ed34abadfb5258c2e9aeda10
Pranav K <pranavkcse@gmail.com>
2023-06-19 12:52:39 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
-				team: resolve(__dirname, 'team', 'index.html'),
+				about: resolve(__dirname, 'about', 'index.html'),
 			},
 		},
 	},

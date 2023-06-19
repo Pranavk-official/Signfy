4d83335ecd2a7f056ae9e4424ade38e482c05d8d
Pranav K <pranavkcse@gmail.com>
2023-06-19 12:39:01 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about', 'index.html'),
-      },
-    },
-  },
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				about: resolve(__dirname, 'about', 'index.html'),
+			},
+		},
+	},
 })
